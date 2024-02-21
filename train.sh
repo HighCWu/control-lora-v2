b7fbef6016f676d667f2d5aac274082d72121f85
highcwu <highcwu@163.com>
2024-02-21 14:03:20 +0800

    add train code for head3d

--- train.sh
+++ train.sh
@@ -3,3 +3,4 @@ accelerate launch -m train.tutorial
 # accelerate launch -m train.tutorial_same_level_latent
 # accelerate launch -m train.tutorial_dora
 # accelerate launch -m train.face_landmarks
+# accelerate launch -m train.head3d
